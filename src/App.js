10f52a6b8a4c3d695293069f90aafcdef9a471b3
tianyongchao <51394548+tianyongchao@users.noreply.github.com>
2021-06-21 18:14:48 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      </header>
-    </div>
-  );
+import React, {Component} from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Login from './pages/login/login'
+export default class App extends Component {
+  render () {
+    return (
+      <Router>
+        <Switch>
+          <Route path='/login' component={Login}/>
+          <Route path='/' component={Admin}/>
+        </Switch>
+      </Router>
+    )
+  }
 }
-
-export default App;
